d753a0a6d4e513f585795ea121c07d075ad4f3fb
Naykuu <faureandrew61@gmail.com>
2018-01-05 12:17:18 +0100

    Creation Class Coordonnees
    Ajout Getters, Setters, constructeur par defaut ainsi que deux variables X et Y

---
+++ Coordonnees.java
@@ -0,0 +1,29 @@
+/**
+ *
+ * @author Naykuu
+ * @version 1.0
+ */
+public class Coordonnees
+{
+	//attributs protegés
+	protected long x;
+	protected long y;
+	
+	//setter
+	public void setX(long x);
+	public void setY(long y);
+	
+	//getter
+	public void getX (long x);
+	public void getY (long y);
+	
+	//Constructeur default
+	public Coordonnees()	
+	
+	
+	public void ajouter(Coordonnees Autre)
+	{
+		
+	}
+	
+}
