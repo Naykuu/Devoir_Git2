291bab08cef61fe86c11d167972dff57d63659d0
Naykuu <faureandrew61@gmail.com>
2018-01-05 11:41:32 +0100

    Ajout valeurs aux variables et methode main d'affichage de ces dernières

--- Main.java
+++ Main.java
@@ -5,10 +5,18 @@
  */
 public class Main
 {
+	public long gauche = 155;
+	public long droite = 200;
 	
-	public long gauche;
-	public long droite;
-	  
+	/**
+     * Constructeur d'objets de classe Main
+     */
+    public static void Main(String args[])
+    {
+        System.out.println(gauche);
+        System.out.println(Droite);
+    }
+	
 	/**
 	 * 
 	 * Creation methode statique 
