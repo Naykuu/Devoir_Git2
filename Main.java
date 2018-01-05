489fc4299026be22a9f1167918c700724a6dea1a
Naykuu <faureandrew61@gmail.com>
2018-01-05 11:36:00 +0100

    Ajout Methode statique AjoutGaucheDroite dans Main

---
+++ Main.java
@@ -0,0 +1,18 @@
+/**
+ *
+ * @author Naykuu
+ * @version 1.0
+ */
+public class Main
+{
+	
+	/**
+	 * 
+	 * Creation methode statique 
+	 */
+	 public static void ajoutDroiteGauche()
+	 {
+		gauche = gauche + droite;
+	 }
+	 
+}
